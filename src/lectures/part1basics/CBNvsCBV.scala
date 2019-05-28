aaf0b5313cd510bec1ac96cee6877bd7d37d2032
Htet Naing <htet.naing91@gmail.com>
2019-05-28 20:15:51 +0800

    Initial Commit

---
+++ src/lectures/part1basics/CBNvsCBV.scala
@@ -0,0 +1,30 @@
+package lectures.part1basics
+
+object CBNvsCBV extends App {
+
+  def callByValue(x: Long): Unit = {
+    println("by value: " + x)
+    println("by value: " + x)
+  }
+
+  def callByName(x: => Long): Unit = {
+    println("by value: " + x)
+    println("by value: " + x)
+  }
+
+  callByValue(System.nanoTime())
+  callByName(System.nanoTime())
+
+  def infinite() : Int = 1 + infinite()
+  def printFirst(x: Int, y: => Int)= println(x)
+
+  printFirst(infinite(), 34)
+
+  // Call by value:
+  // value is computed before call
+  // same value used everywhere
+
+  // Call by name:
+  // expression is passed literally
+  // expression is evaluated at every use within
+}
