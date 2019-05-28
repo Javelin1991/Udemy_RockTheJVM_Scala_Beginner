67a253904b0db2893ae8421b8547b48f24565a9b
Htet Naing <htet.naing91@gmail.com>
2019-05-28 20:56:03 +0800

    Save changes for gitignore

--- src/lectures/part1basics/StringOps.scala
+++ src/lectures/part1basics/StringOps.scala
@@ -18,12 +18,23 @@ object StringOps extends App {
   println(str.reverse)
   println(str.take(2))
 
-  // Scala-specific
+  // Scala-specific: String interpolators
+
+  // S-interpolators
   val name = "David"
   val age = 12
   val greeting = s"Hello, my name is $name and I am $age years old"
   val anotherGreeting = s"Hello, my name is $name and I will be turning ${age + 1} years old"
   println(anotherGreeting)
 
+  // F-interpolators
+  val speed = 1.2f
+  val myth = f"$name can eat $speed%2.2f burgers per minute"
+  println(myth)
+
+  // raw-interpolator
+  println(raw"This is a \n newline")
+  val escaped = "This is a \n newline"
+  println(raw"$escaped")
 
 }
