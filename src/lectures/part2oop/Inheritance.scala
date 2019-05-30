1fc1902d79882b54a0853045124f831e5e4633b8
Htet Naing <htet.naing91@gmail.com>
2019-05-30 16:29:57 +0800

    Save changes

---
+++ src/lectures/part2oop/Inheritance.scala
@@ -0,0 +1,55 @@
+package lectures.part2oop
+
+object Inheritance extends App {
+
+  sealed class Animal {
+    val creatureType = "wild"
+    def eat = println("nomnom")
+  }
+
+  class Cat extends Animal {
+    def crunch = {
+      eat
+      println("crunch crunch")
+    }
+  }
+
+  val cat = new Cat
+  cat.crunch
+
+  // constructors
+  class Person(name: String, age: Int) {
+    def this(name: String) = this(name, 0)
+  }
+  class Adult(name: String, age: Int, idCard: String) extends Person(name)
+
+  // overriding
+  class Dog(override val creatureType: String) extends Animal {
+//    override val creatureType = "domestic"
+    override def eat = {
+      super.eat
+      println("crunch, crunch")
+    }
+  }
+
+//  class Dog(dogType: String) extends Animal {
+//    override val creatureType: String = dogType
+//  }
+
+  val dog = new Dog("K9")
+  dog.eat
+  println(dog.creatureType)
+
+  // type substitution // polymorphism
+  val unknownAnimal: Animal = new Dog("K9")
+  unknownAnimal.eat
+
+  // overRIDING vs overLOADING
+
+  // super
+
+  // preventing overrides
+  // 1 - use final
+  // 2 - use final on the entire class
+  // 3 - seal the class = extend classes in this FILE, but prevents extension in other files
+}
